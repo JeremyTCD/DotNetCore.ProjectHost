55dd17cabea30d63c9e28ecd741a28f3fd59ea3a
JeremyTCD <jeremytanchongdao@gmail.com>
2017-07-11 21:32:52 +0800

    Added ProjectRunner StubProject

---
+++ test/ProjectRunner.StubProject/StubClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JeremyTCD.ProjectRunner.Tests.StubProject
+{
+    public class StubClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(nameof(StubClass.Main));
+        }
+    }
+}
