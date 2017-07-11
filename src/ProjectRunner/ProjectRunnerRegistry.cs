aa8a36aacfac9040f65edc050779419aa8d0d76d
JeremyTCD <jeremytanchongdao@gmail.com>
2017-07-11 21:32:32 +0800

    Added ProjectRunnerRegistry

---
+++ src/ProjectRunner/ProjectRunnerRegistry.cs
@@ -0,0 +1,26 @@
+﻿using JeremyTCD.DotNetCore.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using StructureMap;
+
+namespace JeremyTCD.ProjectRunner
+{
+    public class ProjectRunnerRegistry : Registry
+    {
+        public ProjectRunnerRegistry()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.
+                AddLogging();
+            this.Populate(services);
+
+            For<IPathService>().Singleton().Use<PathService>();
+            For<IDirectoryService>().Singleton().Use<DirectoryService>();
+            For<IMSBuildService>().Singleton().Use<MSBuildService>();
+            For<IActivatorService>().Singleton().Use<ActivatorService>();
+            For(typeof(ILoggingService<>)).Singleton().Use(typeof(LoggingService<>));
+            For<IProcessService>().Singleton().Use<ProcessService>();
+
+            For<ProjectRunner>().Singleton().Use<ProjectRunner>();
+        }
+    }
+}
