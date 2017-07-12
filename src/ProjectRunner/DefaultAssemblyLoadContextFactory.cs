77a9c1a7a318ee0f1c20432608154ffbad175729
JeremyTCD <jeremytanchongdao@gmail.com>
2017-07-12 15:44:12 +0800

    Added DefaultAssemblyLoadContextFactory

---
+++ src/ProjectRunner/DefaultAssemblyLoadContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Loader;
+
+namespace JeremyTCD.ProjectRunner
+{
+    public class DefaultAssemblyLoadContextFactory : IAssemblyLoadContextFactory
+    {
+        public AssemblyLoadContext CreateAssemblyLoadContext(string directory)
+        {
+            return new DirectoryAssemblyLoadContext(directory);
+        }
+    }
+}
---
+++ src/ProjectRunner/IAssemblyLoadContextFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.Loader;
+
+namespace JeremyTCD.ProjectRunner
+{
+    public interface IAssemblyLoadContextFactory
+    {
+        AssemblyLoadContext CreateAssemblyLoadContext(string directory);
+    }
+}
